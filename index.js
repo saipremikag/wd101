f524643f760360d70041c291e78f196382fe982a
Saipremikag <127975631+saipremikag@users.noreply.github.com>
2023-08-08 19:07:39 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -6,10 +6,41 @@ const dobInput = document.getElementById('dob');
 const acceptTermsInput = document.getElementById('accept-terms');
 const tableBody = document.querySelector('#details-table tbody');
 
+function isNameValid() {
+    return /^[a-zA-Z\s.]+$/.test(nameInput.value);
+}
+
+function isDobValid() {
+    const currentDate = new Date();
+    const dob = new Date(dobInput.value);
+    const age = currentDate.getFullYear() - dob.getFullYear();
+
+    console.log("Age:", age);
+
+    if (age < 18 || age > 55) {
+        const dobError = document.getElementById('dob-error');
+        dobError.textContent = "Age should be between 18 and 55.";
+        return false;
+    } else if (dob.getFullYear() < 1968 || dob.getFullYear() > 2005) {
+        const dobError = document.getElementById('dob-error');
+        dobError.textContent = "Year of birth should be between 1968 and 2005.";
+        return false;
+    } else {
+        const dobError = document.getElementById('dob-error');
+        dobError.textContent = ""; // Clear the error message
+        return true;
+    }
+}
+
+
+function isEmailValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    if (!isNameValid() || !isDobValid()) {
+    if (!isNameValid() || !isDobValid() || !isEmailValid(emailInput.value)) {
         return;
     }
 
@@ -23,6 +54,11 @@ form.addEventListener('submit', function (event) {
     newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${password}</td><td>${dob}</td><td>${acceptedTerms}</td>`;
     tableBody.appendChild(newRow);
 
+    // Save data to local storage
+    const entries = JSON.parse(localStorage.getItem('entries')) || [];
+    entries.push({ name, email, password, dob, acceptedTerms });
+    localStorage.setItem('entries', JSON.stringify(entries));
+
     // Clear the form fields
     nameInput.value = '';
     emailInput.value = '';
@@ -31,22 +67,12 @@ form.addEventListener('submit', function (event) {
     acceptTermsInput.checked = false;
 });
 
-function isNameValid() {
-    return /^[a-zA-Z\s.]+$/.test(nameInput.value);
-}
-
-function isDobValid() {
-    const currentDate = new Date();
-    const dob = new Date(dobInput.value);
-    const age = currentDate.getFullYear() - dob.getFullYear();
-
-    if (age < 18 || age > 55) {
-        const dobError = document.getElementById('dob-error');
-        dobError.textContent = "Age should be between 18 and 55.";
-        return false;
-    } else {
-        const dobError = document.getElementById('dob-error');
-        dobError.textContent = ""; // Clear the error message
-        return true;
-    }
-}
+// Load data from local storage on page load
+window.addEventListener('load', function () {
+    const entries = JSON.parse(localStorage.getItem('entries')) || [];
+    entries.forEach(entry => {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `<td>${entry.name}</td><td>${entry.email}</td><td>${entry.password}</td><td>${entry.dob}</td><td>${entry.acceptedTerms}</td>`;
+        tableBody.appendChild(newRow);
+    });
+});
